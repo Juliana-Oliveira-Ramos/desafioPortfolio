7e287eda2a50835de8364ae1600addbc16ba5bca
Juliana Oliveira Ramos <julianaoliveiramos1993@gmail.com>
2026-09-16 10:24:36 -0300

    Configurando o arquivo conftest

--- automacao/ProjetoAutomacaoSauceDemo/conftest.py
+++ automacao/ProjetoAutomacaoSauceDemo/conftest.py
@@ -18,6 +18,7 @@ def setup_teardown():
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
 
 
 
